751f72c308e7c3d4def4975669c2c82db79a8ee9
phtomic <sebastiaogamer@gmail.com>
2024-11-09 01:34:05 +0000

    Ajustes no framework

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,15 @@
 'use strict';
+import nodeExternals from 'webpack-node-externals';
 
-var nodeExternals = require('webpack-node-externals');
-const JavaScriptObfuscator = require('webpack-obfuscator');
-const obfuscate = new JavaScriptObfuscator({
-    compact: true,
-    controlFlowFlattening: true,
-    controlFlowFlatteningThreshold: 1,
-    deadCodeInjection: true,
-    deadCodeInjectionThreshold: 1,
-    debugProtection: true,
-    debugProtectionInterval: 4000,
-    disableConsoleOutput: false,
-    identifierNamesGenerator: 'hexadecimal',
-    log: true,
-    numbersToExpressions: true,
-    renameGlobals: false,
-    selfDefending: true,
-    simplify: true,
-    splitStrings: true,
-    splitStringsChunkLength: 10,
-    stringArray: true,
-    stringArrayCallsTransform: true,
-    stringArrayEncoding: ['rc4'],
-    stringArrayIndexShift: true,
-    stringArrayIndexesType: [
-        'hexadecimal-number'
-    ],
-    stringArrayRotate: true,
-    stringArrayShuffle: true,
-    stringArrayWrappersCount: 10,
-    stringArrayWrappersChainedCalls: true,
-    stringArrayWrappersParametersMaxCount: 10,
-    stringArrayWrappersType: 'function',
-    stringArrayThreshold: 1,
-    transformObjectKeys: false,
-    unicodeEscapeSequence: true
-}, ['index.js'])
-module.exports = {
-    mode: "production",
-    entry: './build/index.js',
-    output: {
-        filename: './index.js', // <-- Important
-        libraryTarget: 'this' // <-- Important
-    },
-    target: 'node', // <-- Important
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        ...process.env.npm_lifecycle_event=='build'?[]:[obfuscate]
-    ],
-    externals: [nodeExternals()]
+export const mode = "production";
+export const entry = './build/index.js';
+export const output = {
+    filename: './index.js', // <-- Important
+    libraryTarget: 'this' // <-- Important
 };
+export const target = 'node';
+export const resolve = {
+    extensions: ['.ts', '.tsx', '.js']
+};
+export const plugins = [];
+export const externals = [nodeExternals()];
